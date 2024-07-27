820999f01334b1234e6d51209d87b3df31e634a4
LoseUFaith <yifanli130960@icloud.com>
2024-07-27 21:11:28 +1200

    fix missing parentheses

--- update.lua
+++ update.lua
@@ -3,7 +3,7 @@ fs=filesystem
 
 pwd=os.getenv("PWD")
 -- backup the current script
-fs.rename(fs.concat(pwd,"reactor++.lua"),fs.concat(pwd,"reactor++.lua.bak")
+fs.rename(fs.concat(pwd,"reactor++.lua"),fs.concat(pwd,"reactor++.lua.bak"))
 
 -- download the new script
 os.execute("wget https://raw.githubusercontent.com/LoseUFaith/Reactorplusplus/dev/reactor++.lua reactor++.lua")
