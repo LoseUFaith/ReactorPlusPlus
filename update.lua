a5d3da3f1f8deab6a7b615b61a8fc2fa8967f212
LoseUFaith <yifanli130960@icloud.com>
2024-07-27 21:06:49 +1200

    fix error

--- update.lua
+++ update.lua
@@ -1,3 +1,10 @@
-mv reactor++.lua reactor++.lua.old
-wget https://raw.githubusercontent.com/LoseUFaith/Reactorplusplus/dev/reactor++.lua reactor++.lua
+-- Purpose: Update the reactor++ script.
+fs=filesystem
+
+pwd=os.getenv("PWD")
+-- backup the current script
+fs.rename(fs.concat(pwd,"reactor++.lua"),fs.concat(pwd,"reactor++.lua.bak")
+
+-- download the new script
+os.execute("wget https://raw.githubusercontent.com/LoseUFaith/Reactorplusplus/dev/reactor++.lua reactor++.lua")
 print("Update complete!")
